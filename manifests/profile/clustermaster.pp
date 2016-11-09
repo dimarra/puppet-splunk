f4330ba53cf958635789d253335b13228a0e9744
Raul Dimar <rdimar@shelde.com>
2016-11-10 09:20:59 +1100

    test using lookup with different strategies

--- manifests/profile/clustermaster.pp
+++ manifests/profile/clustermaster.pp
@@ -20,6 +20,19 @@
 
 class splunk::profile::clustermaster {
 
+  $clustering_first = lookup("splunk::clustering", String, "first", "default value")
+  notify { $clustering_first: }
+
+  $clustering_deep = lookup("splunk::clustering", String, "deep", "default value")
+  notify { $clustering_deep: }
+
+  $clustering_unique = lookup("splunk::clustering", String, "unique", "default value")
+  notify { $clustering_unique: }
+
+  $clustering_hash = lookup("splunk::clustering", String, "hash", "default value")
+  notify { $clustering_hash: }
+
+
   include splunk
 #  class { 'splunk' :
 #    clustering => { 
