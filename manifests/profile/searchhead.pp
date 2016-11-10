68a3568be1c932bccf1ec6f4834a5b75faff342c
Raul Dimar <rdimar@shelde.com>
2016-11-10 15:05:15 +1100

    initial searchhead.pp

---
+++ manifests/profile/searchhead.pp
@@ -0,0 +1,27 @@
+
+# Class: splunk::profile::searchhead
+#
+# This module manages splunk::profile::searchhead
+#
+# Parameters: none
+#
+# Actions:
+#
+# Requires: see Modulefile
+#
+# Sample Usage:
+#
+# vim: ts=2 sw=2 et
+#
+# == Class: splunk::profile::searchhead
+#
+# The Splunk class takes are of installing and configuring a Splunk component of Search Head
+# based on the provided parameters. 
+
+class splunk::profile::searchhead {
+
+  class { 'splunk' :
+    clustering => lookup("splunk::shclustering", Hash[String, Scalar], "hash", undef),
+  }
+
+}
