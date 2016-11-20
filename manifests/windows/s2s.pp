17b97497913b73e27faa39c9291109ab72b35b99
Raul Dimar <rdimar@shelde.com>
2016-11-20 21:49:05 +1100

    enclose in quotes '

--- manifests/windows/s2s.pp
+++ manifests/windows/s2s.pp
@@ -17,7 +17,7 @@ class splunk::windows::s2s (
     recurse => true,
   } ->
   exec { 'openssl dhparam':
-    command   => "openssl dhparam -outform PEM -out ${splunk_home}/etc/auth/certs/dhparam.pem ${dhparamsize}",
+    command   => "openssl dhparam -outform PEM -out '${splunk_home}/etc/auth/certs/dhparam.pem' ${dhparamsize}",
     path      => ['/bin', '/sbin', '/usr/bin', '/usr/sbin', "${splunk_home}/bin"],
     creates   => [
       "${splunk_home}/etc/auth/certs/dhparam.pem",
@@ -29,14 +29,14 @@ class splunk::windows::s2s (
 
   # reuse certs from commercial Puppet
   exec { 'openssl s2s ca commercial puppet':
-    command => "type ${::confdir}/ssl/certs/ca.pem > ${splunk_home}/etc/auth/certs/ca.crt",
+    command => "type ${::confdir}/ssl/certs/ca.pem > '${splunk_home}/etc/auth/certs/ca.crt'",
     path    => ['/bin', '/sbin', '/usr/bin', '/usr/sbin', "${splunk_home}/bin"],
     creates => [ "${splunk_home}/etc/auth/certs/ca.crt", ],
     require => File["${splunk_home}/etc/auth/certs"],
     onlyif  => "if exist ${::confdir}/ssl (exit 0) else (exit 1)"
   } ->
   exec { 'openssl s2s 1 commercial puppet':
-    command => "type ${::confdir}/ssl/private_keys/${::fqdn}.pem ${::confdir}/ssl/certs/${::fqdn}.pem > ${splunk_home}/etc/auth/certs/s2s.pem",
+    command => "type ${::confdir}/ssl/private_keys/${::fqdn}.pem ${::confdir}/ssl/certs/${::fqdn}.pem > '${splunk_home}/etc/auth/certs/s2s.pem'",
     path    => ['/bin', '/sbin', '/usr/bin', '/usr/sbin',],
     creates => [ "${splunk_home}/etc/auth/certs/s2s.pem", ],
     onlyif  => "if exist ${::confdir}/ssl (exit 0) else (exit 1)"
