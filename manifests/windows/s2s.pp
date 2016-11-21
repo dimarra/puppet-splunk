7d1c1c9954be4def888c0a3ff96affc51dc664d2
Raul Dimar <rdimar@shelde.com>
2016-11-21 14:15:37 +1100

    test with concat

--- manifests/windows/s2s.pp
+++ manifests/windows/s2s.pp
@@ -17,8 +17,8 @@ class splunk::windows::s2s (
     recurse => true,
   } ->
   exec { 'openssl dhparam':
-    command   => "${::system32}\\cmd.exe /c \"openssl dhparam -outform PEM -out '${splunk_home}/etc/auth/certs/dhparam.pem' ${dhparamsize}\"",
-    path      => ["${::system32}", "${splunk_home}/bin"],
+    command   => "openssl dhparam -outform PEM -out \"${splunk_home}/etc/auth/certs/dhparam.pem\" ${dhparamsize}",
+    path      => ["${::system32}", "${splunk_home}/bin", ],
     creates   => [
       "${splunk_home}/etc/auth/certs/dhparam.pem",
     ],
@@ -26,7 +26,7 @@ class splunk::windows::s2s (
     logoutput => true,
     timeout   => 900,
   }
-
+/*
   # reuse certs from commercial Puppet
   exec { 'openssl s2s ca commercial puppet':
     command => "type ${::confdir}/ssl/certs/ca.pem > '${splunk_home}/etc/auth/certs/ca.crt'",
@@ -41,7 +41,32 @@ class splunk::windows::s2s (
     creates => [ "${splunk_home}/etc/auth/certs/s2s.pem", ],
     onlyif  => "${::system32}\\cmd.exe /c 'if exist ${::confdir}/ssl (exit 0) else (exit 1)'"
   }
-  
+ */  
+  file { "${splunk_home}/etc/auth/certs/ca.crt": 
+    ensure  => 'present',
+    replace => 'no',
+    owner   => $splunk_os_user,
+    group   => $splunk_os_user,
+    source  => "${::confdir}/ssl/certs/ca.pem",
+  }
+
+  concat { "${splunk_home}/etc/auth/certs/s2s.pem":
+    owner   => $splunk_os_user,
+    group   => $splunk_os_user,
+    mode    => '0644'
+  }
+
+  concat::fragment{ "${::confdir}/ssl/private_keys/${::fqdn}.pem":
+    target  => "${splunk_home}/etc/auth/certs/s2s.pem",
+    source  => "${::confdir}/ssl/private_keys/${::fqdn}.pem",
+    order   => '01'
+  }
+
+  concat::fragment{ "${::confdir}/ssl/certs/${::fqdn}.pem":
+    target  => "${splunk_home}/etc/auth/certs/s2s.pem",
+    source  => "${::confdir}/ssl/certs/${::fqdn}.pem",
+    order   => '02'
+  }
   } # if $certtype
 
 }
