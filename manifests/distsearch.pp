48e44672c981bb7aeb9baeb0458787d6f8f0c73f
Raul Dimar <rdimar@shelde.com>
2016-11-23 15:36:02 +1100

    notify on searchpeers

--- manifests/distsearch.pp
+++ manifests/distsearch.pp
@@ -5,6 +5,7 @@ class splunk::distsearch (
   $splunk_os_user = $splunk::splunk_os_user,
   $splunk_home = $splunk::splunk_home
 ){
+  notify { "distsearch $searchpeers": }
   if $searchpeers == undef {
     file { "${splunk_home}/etc/system/local/distsearch.conf":
       ensure  => 'absent',
